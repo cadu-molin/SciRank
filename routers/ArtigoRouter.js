8e47df9f0ffd51d1ba5a8ba1f6b4fd95a7b837c9
Jose Henrique Biavati Cagnini <jose.cagnini@ciss.com.br>
2024-04-30 17:36:26 -0300

    artigo list

--- routers/ArtigoRouter.js
+++ routers/ArtigoRouter.js
@@ -5,6 +5,8 @@ const controllerArtigo = require('../controllers/controllerArtigo')
 
 router.get("/create", controllerArtigo.getCreate)
 router.post("/create", controllerArtigo.postCreate)
+router.get('/delete/:idArtigo', controllerArtigo.getDelete)
+router.get('/update/:idArtigo', controllerArtigo.getUpdate)
 
 router.get("/list", controllerArtigo.getList)
 
--- seeders/20240422023048-usuario_padrao.js
+++ seeders/20240422023048-usuario_padrao.js
@@ -11,6 +11,48 @@ module.exports = {
         usuario:"Admin",
         senha: "123"
       },
+      {
+        nome:"autor1",
+        email:"autor1@gamil.com",
+        tipousuario: 1,
+        usuario:"autor1",
+        senha: "1"
+      },
+      {
+        nome:"autor2",
+        email:"autor2@gamil.com",
+        tipousuario: 1,
+        usuario:"autor2",
+        senha: "1"
+      },
+      {
+        nome:"autor3",
+        email:"autor3@gamil.com",
+        tipousuario: 1,
+        usuario:"autor3",
+        senha: "1"
+      },
+      {
+        nome:"autor4",
+        email:"autor4@gamil.com",
+        tipousuario: 1,
+        usuario:"autor4",
+        senha: "1"
+      },
+      {
+        nome:"autor5",
+        email:"autor5@gamil.com",
+        tipousuario: 1,
+        usuario:"autor5",
+        senha: "1"
+      },
+      {
+        nome:"autor6",
+        email:"autor6@gamil.com",
+        tipousuario: 1,
+        usuario:"autor6",
+        senha: "1"
+      },
     ])
   },
 
